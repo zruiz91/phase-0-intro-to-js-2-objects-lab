70a2d5f72f6768e43070355f83d5cd32358a3762
Zach Ruiz <powersbooth10@gmail.com>
2022-06-29 22:41:08 -0500

    commit tree 091226c0a59bc80f68a6d45c856c9f9d16fc6385

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution in this file!
+const employee = {
+    name: "Jeff",
+    streetAddress: "4444 four street",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updatedEmployee = {
+        ...employee,
+        [key]: value,
+    }
+    return updatedEmployee;
+};
+
+updateEmployeeWithKeyAndValue;
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+};
+
+destructivelyUpdateEmployeeWithKeyAndValue;
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee};
+    delete newEmployee[key];
+    return newEmployee;
+};
+
+deleteFromEmployeeByKey;
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+};
+
+destructivelyDeleteFromEmployeeByKey;
